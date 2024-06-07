58e28a513a3c402210cbdcd7ac683e8873be5ad6
Your Name <you@example.com>
2024-06-07 12:27:41 +0530

    #4.2, validation for username & password on login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,28 +1,52 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import api from '../api/api'
 import { useNavigate } from 'react-router-dom';
 
 
-const Login = ({ setLoggedIn }) => {
-  
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/auth', { user: username, pwd: password });
+      console.log('token', response.data.accessToken);
+      setUsername('');
+      setPassword('');
+      setError('');
+    } catch (err) {
+      if (!err.response) {
+        setError('No Server Response');
+      } else if (err.response.status === 400) {
+        setError('Missing Username or Password');
+      } else if (err.response.status === 401) {
+        setError('Unauthorized');
+      } else {
+        setError(err.response.data?.message || 'Login Failed');
+      }
+    }
+  };
+
   return (
-    <form className="login-form" >
+    <form className="login-form" onSubmit={handleLogin}>
       <h2>Login</h2>
       <input 
         type="text" 
-        value="" 
-        onChange={()=>{}} 
+        value={username} 
+        onChange={(e) => setUsername(e.target.value)} 
         placeholder="Username" 
       />
       <input 
         type="password" 
-        value=""
-        onChange={()=>{}} 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} 
         placeholder="Password" 
       />
       <button type='submit'>Login</button>
+      {error && <p>{error}</p>}
     </form>
   );
 };
